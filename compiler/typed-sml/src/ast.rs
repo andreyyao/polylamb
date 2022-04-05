b990d93324e2e53b11e5b2ed33c83a9c978bcd96
Andrey Yao <awy32@cornell.edu>
2022-04-05 19:38:59 -0400

    Further AST edits

--- compiler/typed-sml/src/ast.rs
+++ compiler/typed-sml/src/ast.rs
@@ -1,28 +1,50 @@
+type Id = String;
+type Tipe = String;
 
-type Id<'a> = &'a str;
-type Tipe<'a> = &'a str;
+/* Parameters, like [a : t]*/
+pub struct Param { id: Id, typ: Tipe }
 
+//Literals
 pub enum Constant {
     Integer(i64),
     Boolean(bool)
 }
 
-pub enum Binop {
+pub enum Unary {
+    Not, Neg
+}
+
+pub enum Binary {
     Add, Sub, Mul, Div,
     Eq, Lt, Gt, Le, Ge, Ne,
     Andalso, Orelse
 }
 
-pub enum Unop {
-    Not, Neg
+
+pub enum Data {
+    Const{ constnt: Constant },
+    Unop{ op: Unary, kid: Box<Expr> },
+    Binop{ op: Binary, lhs: Box<Expr>, rhs: Box<Expr> },
+    Lambda{ args: Vec<Param>, body: Box<Expr> },
+    Branch{ cond: Box<Expr>, br_t: Box<Expr>, br_f: Box<Expr> },
+    Let { arg: Param, exp: Box<Expr>, env: Box<Expr> }
 }
 
-pub enum Data<'a> {
-    Const(Constant),
-    UnoExpr{ op: Unop, kid: Box<Expr<'a>> },
-    BinExpr{ op: Binop, lhs: Box<Expr<'a>>, rhs: Box<Expr<'a>> },
-    Lambda{ args: Vec<Id<'a>>, body: Box<Expr<'a>> },
-    Branch{ cond: Box<Expr<'a>>, br_t: Box<Expr<'a>>, br_f: Box<Expr<'a>> }
-}
 
-pub struct Expr<'a> { exp: Data<'a>, typ: Tipe<'a> }
+pub struct Expr { exp: Data, typ: Tipe }
+
+
+impl Data {
+
+    /* Returns an immutable vector of immediate children */
+    fn children(&self) -> Vec<&Expr> {
+	match self {
+	    Data::Const{ constnt: _ } => vec![],
+	    Data::Unop{ op: _, kid} => vec![kid],
+	    Data::Binop{ op: _, lhs, rhs } => vec![lhs, rhs],
+	    Data::Lambda{ args: _, body } => vec![body],
+	    Data::Branch{ cond, br_t, br_f } =>vec![cond, br_t, br_f],
+	    Data::Let { arg: _, exp, env } => vec![exp, env]
+	}
+    }
+}
