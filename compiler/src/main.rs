f1bf720b3af0103a06eca0e965525a3fc80b331c
Andrey Yao <awy32@cornell.edu>
2023-04-16 17:51:14 -0400

    Modified testing from main using cargo run

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -3,17 +3,11 @@ use annotate_snippets::snippet::{Annotation, Slice, Snippet};
 use compiler::system_f::parse::parse_prog;
 use compiler::system_f::semant::check_prog;
 
-const PROGRAM: &str = "let all: Int -> Int -> (Int -> Bool) -> Bool =
-  lambda min: Int. lambda max: Int. lambda pred: Int -> Bool.
-    let folder: Int -> Bool -> Bool =
-      lambda element: Int. lambda acc: Bool.
-        if element - 1 < max
-        then (pred element) && true
-        else b + acc in
-    folder min true";
+const SOURCE: &str = "tests/progs/simple.polylamb";
 
 fn main() {
-    let prog = parse_prog(PROGRAM).unwrap();
+    let program = std::fs::read_to_string(SOURCE).expect("Hmm");
+    let prog = parse_prog(&program).unwrap();
     let result = check_prog(&prog);
     let err = result.unwrap_err();
     let snippet = Snippet {
@@ -24,7 +18,7 @@ fn main() {
         }),
         footer: vec![],
         slices: vec![Slice {
-            source: PROGRAM,
+            source: &program,
             line_start: 1, // TODO
             origin: None,
             annotations: err.annotations,
